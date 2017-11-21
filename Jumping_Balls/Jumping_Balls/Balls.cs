ca1037d178c92150a78e14a287b889eb7b922593
SOLOMIIA\Соломія <solomiiabench@gmail.com>
2017-11-21 13:01:45 +0200

    doing many balls

--- Jumping_Balls/Jumping_Balls/Balls.cs
+++ Jumping_Balls/Jumping_Balls/Balls.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Jumping_Balls
 {
     class Balls
     {
+        Brush brush_color = Brushes.BlueViolet;
+        float size = 50;
+        int x_coord = 0;
+        int y_coord = 0;
+        Size formSize;
+
+        int xv_coord = 3;
+        int yv_coord = 3;
+
+        public Balls(Size _formSize)
+        {
+            this.formSize = _formSize;
+        }
+
+        public Balls(Brush _brush_color, float _size, int _x_coord, int _y_coord, Size _formSize)
+        {
+            this.brush_color = _brush_color;
+            this.size = _size;
+            this.x_coord = _x_coord;
+            this.y_coord = _y_coord;
+            this.formSize = _formSize;
+        }
+
+        public void draw(Graphics g1)
+        {
+            Pen p = new Pen(brush_color, size);
+            g1.DrawEllipse(p, x_coord, y_coord, 10, 10);
+            return;
+        }
+
+        public void change_position()
+        {
+            x_coord += xv_coord;
+            y_coord += yv_coord;
+
+            if (x_coord <= 0)
+            {
+                xv_coord = -xv_coord;
+            }
+
+            if (y_coord <= 0)
+            {
+                yv_coord = -yv_coord;
+            }
+
+            if (x_coord > formSize.Width - 50)
+            {
+                xv_coord = -xv_coord;
+            }
+
+            if (y_coord > formSize.Height - 70)
+            {
+                yv_coord = -yv_coord;
+            }
+        }
     }
 }
--- Jumping_Balls/Jumping_Balls/Form1.cs
+++ Jumping_Balls/Jumping_Balls/Form1.cs
@@ -1,56 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Jumping_Balls
 {
     public partial class Form1 : Form
     {
+        Balls b1;
+        Balls[] ballArr = new Balls[70];
+        Random rnd = new Random();
+        Timer timer = new Timer();
+
         public Form1()
         {
             InitializeComponent();
+            DoubleBuffered = true;
+
+            for (int i = 0; i < ballArr.Count(); i++)
+            {
+                Size size = new Size(rnd.Next(1, 100), rnd.Next(1, 100));
+
+                Brush result = Brushes.Transparent;
+                Type brushesType = typeof(Brushes);
+                PropertyInfo[] properties = brushesType.GetProperties();
+                int Random = rnd.Next(properties.Length);
+                result = (Brush)properties[Random].GetValue(null, null);
+
+                int X_rand_coord = rnd.Next(1, Width - 50);
+                int y_rand_coord = rnd.Next(1, Height - 70);
+                ballArr[i] = new Balls(result, 50, X_rand_coord, y_rand_coord, Size);
+            }
+
+            b1 = new Balls(Size);
         }
-
-        Timer Timer = new Timer();
-
-        int x, y;
-        int xvelocity = 50;
-        int yvelocity = 50;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Timer.Interval = 50;
-            Timer.Tick += Timer_Tick;
-            Timer.Start();
+            timer.Interval = 5;
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            x += xvelocity;
-            if ((x > Width - xvelocity - 80) || (x < 0))
+            for (int i = 0; i < ballArr.Count(); i++)
             {
-                xvelocity = -xvelocity;
+                ballArr[i].change_position();
             }
-
-            y += yvelocity;
-            if ((y > Height - yvelocity - 80) || (y < 0))
-            {
-                yvelocity = -yvelocity;
-            }
-
             Refresh();
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            e.Graphics.DrawEllipse(new Pen(Brushes.BlueViolet, 10), x, y, 80, 80);
+
+            for (int i = 0; i < ballArr.Count(); i++)
+            {
+                ballArr[i].draw(e.Graphics);
+            }
         }
     }
 }
